9183f4cd9f2e087c559dd48705074bb80800b69d
Gabriel Baptista <biellz1221@gmail.com>
2023-02-04 15:07:54 -0300

    ajustinhos

--- lib/controller/assists_controller.dart
+++ lib/controller/assists_controller.dart
@@ -1,8 +1,8 @@
 import 'package:abc_tech_app/model/assist.dart';
 import 'package:abc_tech_app/service/assist_service.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:get/utils.dart';
+// import 'package:get/get_state_manager/get_state_manager.dart';
+// import 'package:get/utils.dart';
 
 class AssistsController extends GetxController with StateMixin<List<Assist>> {
   late AssistService _assistService;
